c51602d9f8378ed3eabf00d4796337a679c9d08d
felipaff <felipaff@gmail.com>
2023-10-16 18:16:28 -0300

    Initial commit

---
+++ src/main/java/com/assist/control/assistcontrolbackend/AssistControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.assist.control.assistcontrolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistControlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistControlBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assist/control/assistcontrolbackend/AssistControlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assist.control.assistcontrolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistControlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
